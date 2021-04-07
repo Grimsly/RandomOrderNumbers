ca01e3696be333f962d1a2ca535043fc29e18ed3
Grimsly <xmeng.low@gmail.com>
2021-04-07 07:54:58 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomOrderNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a list of integeres from 1 to 10000
+            List<int> list = Enumerable.Range(1, 10000).ToList();
+            // Shuffle the list with the Fisher-Yates shuffle
+            Shuffle.Run(list);
+
+            // Print out the results
+            string output = string.Join(", ", list);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Shuffle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomOrderNumbers
+{
+    class Shuffle
+    {
+        // The function uses the Fisher-Yates shuffle and can take in whatever type of lists
+        public static void Run<T>(List<T> list)
+        {
+            Random rand = new Random();
+            // The shuffle works by first starting at the end of the list
+            // Once a random number has been found in between the current index and 0
+            // Swap the current index's value with the random index's value
+            // This is to prevent bias with the same number being constantly swapped and making the same permutations again and again
+            // When it reaches the start of the list, the loop stops as the only element at the start of the list that can swap with it is itself
+            for (int i = list.Count-1; i > 0; i--)
+            {
+                int randomIndex = rand.Next(i);
+                T temp = list[randomIndex];
+                list[randomIndex] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+}
